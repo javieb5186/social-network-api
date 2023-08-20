d7679089601716095b5e6595905751ed70bc4429
javieb5186 <javieb5186@gmail.com>
2023-08-20 12:45:00 -0600

    Add models and connection to MDB

---
+++ develop/models/User.js
@@ -0,0 +1,51 @@
+const {model, Schema} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+          validator: function(e) {
+            const regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+            const result = regex.test(e);
+            return result;
+          },
+          message: 'You must provide a valid email.',
+        },
+      },
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'thought',
+        },
+      ],
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: this,
+        },
+      ],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    },
+);
+
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const db = require('./develop/config/connection');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+  });
+});
