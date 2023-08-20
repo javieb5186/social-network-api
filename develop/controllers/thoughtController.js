65a4e9818077c99252e95bd32b36026eb8273858
javieb5186 <javieb5186@gmail.com>
2023-08-20 16:05:22 -0600

    Add working thought and reaction routes

---
+++ develop/controllers/thoughtController.js
@@ -0,0 +1,75 @@
+const {User, Thought} = require('../models');
+
+module.exports = {
+  async getThoughts(req, res) {
+    try {
+      const thought = await Thought.find();
+      res.json(thought);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  async getThoughtsById(req, res) {
+    try {
+      const thought = await Thought.findById(req.params._id);
+      res.json(thought);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  async createThought(req, res) {
+    try {
+      const thought = await Thought.create(req.body);
+      const user = await User.findOne({username: req.body.username});
+      console.log(user);
+      user.thoughts.push(thought._id);
+      await user.save();
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  async updateThought(req, res) {
+    try {
+      const thought = await Thought.findByIdAndUpdate(req.params._id, 
+        {$set: req.body},
+      );
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async deleteThought(req, res) {
+    try {
+      const thought = await Thought.findByIdAndDelete(req.params._id);
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async createReaction(req, res) {
+    try {
+      const thought = await Thought.findById(req.params.thoughtId);
+      thought.reactions.push(req.body);
+      await thought.save();
+      res.json({message: 'Reaction Created'});
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  async deleteReaction(req, res) {
+    try {
+      const thought = await Thought.findById(req.params.thoughtId);
+      thought.reactions.pull(req.body.reactionId);
+      await thought.save();
+      res.json({message: 'Reaction Deleted'});
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+}
--- develop/routes/api/users.js
+++ develop/routes/api/users.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-const {getUsers, createUser, updateUser, deleteUser, addFriend, deleteFriend} = require('../../controllers/userController');
+const {getUsers, createUser, getUserById, updateUser, deleteUser, addFriend, deleteFriend} = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(createUser);
-router.route('/:_id').put(updateUser).delete(deleteUser);
+router.route('/:_id').get(getUserById).put(updateUser).delete(deleteUser);
 router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
